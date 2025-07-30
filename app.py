2e747c7f29d41995cc92fe9af943f9c66c04fee3
A Suchit <117534727+Suchit070504@users.noreply.github.com>
2025-07-31 00:47:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", method=['GET'])
+@app.route("/", methods=['GET'])
 def home():
-  return "<h1>Suchit's Flask App</h1>"
+    return "<h1>Arpan's Flask App</h1>"
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
