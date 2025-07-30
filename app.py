e274ab6f0bdf5df45072487f6361b5cb12364ab3
A Suchit <117534727+Suchit070504@users.noreply.github.com>
2025-07-31 00:49:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
-    return "<h1>Arpan's Flask App</h1>"
+    return "<h1>Suchit's Flask App</h1>"
 
 if __name__ == "__main__":
     app.run()
